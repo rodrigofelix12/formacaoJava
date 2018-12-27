abccd3ca214016c168e0e047725a7830a24e8e8a
Rodrigo Felix <rodrigo.felix321@gmail.com>
2018-12-26 23:14:21 -0200

    Finalizando a aula 06 sobre interfaces

---
+++ bytebank-herdado/src/Administrador.java
@@ -0,0 +1,27 @@
+
+public abstract class Administrador extends Funcionario implements Autenticavel {
+
+	private int senha;
+	
+	@Override
+	public double getBonificacao() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public boolean autentica(int senha) {
+
+		if (this.senha == senha) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,4 +1,4 @@
-public class Gerente extends Funcionario {
+public class Gerente extends Funcionario implements Autenticavel {
 	
 	private int senha;
 
@@ -7,16 +7,19 @@ public class Gerente extends Funcionario {
 		return super.getSalario();
 	}
 
-	public void setSenha(int senha) {
-		this.senha = senha;
-	}
+	 @Override
+	    public void setSenha(int senha){
+	        this.senha = senha;
+	    }
 
-	public boolean autentica(int senha) {
-		if(this.senha == senha){
-			return true;
-		} else {
-			return false;
-		}
-	}
+	    @Override
+	    public boolean autentica(int senha){
+
+	        if(this.senha == senha){
+	            return true;
+	        } else {
+	            return false;
+	        }
+	    }
 
 }
