62cdc5d6b28153c3d6d6da06915dfb319191f154
Rodrigo Felix <rodrigo.felix321@gmail.com>
2019-01-09 21:52:36 -0200

    Fim da aula 4 checked e unchecked

--- java-pilha/src/FluxoComError.java
+++ java-pilha/src/FluxoComError.java
@@ -1,4 +1,4 @@
-public class Fluxo {
+public class FluxoComError {
 
 	public static void main(String[] args) {
 		System.out.println("Ini do main");
@@ -12,18 +12,16 @@ public class Fluxo {
 		System.out.println("Fim do main");
 	}
 
-	private static void metodo1() throws MinhaExcecao {
+	private static void metodo1() {
 		System.out.println("Ini do metodo1");
 		metodo2();
 		System.out.println("Fim do metodo1");
 	}
 
-	private static void metodo2() throws MinhaExcecao {
-		System.out.println("Ini do metodo2");
-		
-		throw new MinhaExcecao("deu muito errado");
-		
-//		System.out.println("Fim do metodo2");		
+	private static void metodo2() {
+		System.out.println("ini do metodo 2");
+		metodo2();
+		System.out.println("fim do metodo 2");
 	}
 }
 
